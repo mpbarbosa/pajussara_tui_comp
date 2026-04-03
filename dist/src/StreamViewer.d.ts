cb90d5baed8c466e9be82479e30846b616b40d74
Marcelo Pereira Barbosa <mpbarbosa@gmail.com>
2026-04-03 20:28:34 -0300

    chore: build artifacts for v1.1.6

--- dist/src/StreamViewer.d.ts
+++ dist/src/StreamViewer.d.ts
@@ -12,7 +12,7 @@
  * - Pure display logic only; all state lives in the parent (streamChunks prop)
  * - This component is the impure boundary (keyboard input)
  *
- * @version 1.0.0
+ * @version 1.1.6
  * @since 2026-03-10
  */
 import React from 'react';
